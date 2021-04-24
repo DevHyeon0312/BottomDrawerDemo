4d8bba00c3fb86d24c42cb07044248b09016dd74
devhyeon <devhyeon0312@gmail.com>
2021-04-25 01:53:43 +0900

    BottomDrawer Demo

--- app/src/main/java/com/bottomupdrawer/demo/MainActivity.kt
+++ app/src/main/java/com/bottomupdrawer/demo/MainActivity.kt
@@ -2,6 +2,9 @@ package com.bottomupdrawer.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import lib.bottomupdrawer.BasicBottomSheet
 
 class MainActivity : AppCompatActivity() {
@@ -13,21 +16,21 @@ class MainActivity : AppCompatActivity() {
         val titleFragment = TitleFragment()
         val bodyFragment = BodyFragment()
 
-//        val callback = (object : BottomSheetBehavior.BottomSheetCallback() {
-//            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-//            }
-//
-//            override fun onStateChanged(bottomSheet: View, newState: Int) {
-//                when (newState) {
-//                    BottomSheetBehavior.STATE_COLLAPSED -> Toast.makeText(this@BasicActivity, "STATE_COLLAPSED", Toast.LENGTH_SHORT).show()
-//                    BottomSheetBehavior.STATE_EXPANDED -> Toast.makeText(this@BasicActivity, "STATE_EXPANDED", Toast.LENGTH_SHORT).show()
-//                    BottomSheetBehavior.STATE_DRAGGING -> Toast.makeText(this@BasicActivity, "STATE_DRAGGING", Toast.LENGTH_SHORT).show()
-//                    BottomSheetBehavior.STATE_SETTLING -> Toast.makeText(this@BasicActivity, "STATE_SETTLING", Toast.LENGTH_SHORT).show()
-//                    BottomSheetBehavior.STATE_HIDDEN -> Toast.makeText(this@BasicActivity, "STATE_HIDDEN", Toast.LENGTH_SHORT).show()
-//                    else -> Toast.makeText(this@BasicActivity, "OTHER_STATE", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//        })
+        val callback = (object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {}
+                    BottomSheetBehavior.STATE_EXPANDED -> {}
+                    BottomSheetBehavior.STATE_DRAGGING -> {}
+                    BottomSheetBehavior.STATE_SETTLING -> {}
+                    BottomSheetBehavior.STATE_HIDDEN -> {}
+                    else -> {}
+                }
+            }
+        })
 
         BasicBottomSheet
                 .Begin(activity = this@MainActivity)
@@ -35,7 +38,7 @@ class MainActivity : AppCompatActivity() {
                 .peekHeight(60)
                 .addTitle(titleFragment)
                 .addContents(bodyFragment)
-//            .addCallBack(callback)
+                .addCallBack(callback)
                 .commit()
     }
 }
